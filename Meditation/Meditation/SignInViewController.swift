9cdd3cc2466055bcf2eacb192dd0350604c30525
Alsu <66012600+alsu71@users.noreply.github.com>
2021-11-16 12:22:21 +0300

    Add files via upload

---
+++ Meditation/Meditation/SignInViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  Meditation
+//
+//  Created by Student on 16.11.2021.
+//
+
+import UIKit
+import Alamofire
+import SwiftyJSON
+
+
+
+class SignInViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        //myButton.titleLabel?.font =  UIFont(name: Alegreya, size: 20)
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
